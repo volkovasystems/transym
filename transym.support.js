00824b0a963fa67fa187bad2a6937c914376ddb3
johnlenon <johnlenonmaghanoy@gmail.com>
2017-06-26 12:05:55 +0800

    Updated dependency.

--- transym.support.js
+++ transym.support.js
@@ -35,6 +35,10 @@
               			"module": "transym",
               			"author": "Richeve S. Bebedor",
               			"eMail": "richeve.bebedor@gmail.com",
+              			"contributors": [
+              				"John Lenon Maghanoy <johnlenonmaghanoy@gmail.com>",
+              				"Vinse Vinalon <vinsevinalon@gmail.com>"
+              			],
               			"repository": "https://github.com/volkovasystems/transym.git",
               			"test": "transym-test.js",
               			"global": true
