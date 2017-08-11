f023401bd3ed6b73c4eefbb3bae63d96c9d031a9
johnlenon <johnlenonmaghanoy@gmail.com>
2017-08-11 15:14:26 +0800

    Updated test.

--- transym-test.js
+++ transym-test.js
@@ -3,9 +3,9 @@ const assert = require( "assert" );
 const transym = require( "./transym.js" );
 
 assert.deepEqual( transym( { [ Symbol.for( "hello" ) ]: "test" }, { } ),
-	{ [ Symbol.for( "hello" ) ]: "test" }, "should be deeply equal" );
+	{ [ Symbol.for( "hello" ) ]: "test" }, "should be equal to { [ Symbol.for( 'hello' ) ]: 'test' }" );
 
 assert.deepEqual( transym( { [ Symbol.for( "hello" ) ]: "test" }, { [ Symbol.for( "hello" ) ]: "test" }, true ),
-	{ }, "should be deeply equal" );
+	{ }, "should be equal to { }" );
 
 console.log( "ok" );
